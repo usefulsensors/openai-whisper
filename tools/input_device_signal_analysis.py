b167b8ac55cddd71a38059519e49b29f53aabc59
Guy Nicholson <35084975+guynich@users.noreply.github.com>
2023-04-12 16:42:54 -0700

    Adds improved support for Linux and other changes
    Tested on Ubuntu 22.04 LTS
    
    Changes:
    - added on Linux: default audio device is automatically selected.  This mitigates crash with plotting and crash when choosing sampling rates other than 48000.
    - added assert check on supported format.
    ⁃ added Input Device signal clipping saturation is colored red.
    ⁃ added Software gain argument -g added.
    ⁃ added argument for lower and upper thresholds on crest factor check; upper bound mitigates short explosive sounds like claps and pops
    - added argument for lower and upper bounds.
    ⁃ changed Plotting is enabled by default.
    ⁃ changed zero crossing check is enabled by default.  It is normalized by time only.
    ⁃ updated documentation.

--- tools/input_device_signal_analysis.py
+++ tools/input_device_signal_analysis.py
@@ -1,13 +1,14 @@
 """
 Input device signal analysis with plotting.
 
-This blocks the audio device.
 TODO(guy): make non-blocking version.
 
-Uses pyaudio: https://pypi.org/project/PyAudio/ 
-https://stackoverflow.com/questions/35970282/what-are-chunks-samples-and-frames-when-using-pyaudio
-"""
+Uses pyaudio: https://pypi.org/project/PyAudio/
+See: https://stackoverflow.com/questions/35970282/what-are-chunks-samples-and-frames-when-using-pyaudio
 
+Example command:
+python3 ./input_device_signal_analysis.py
+"""
 import argparse
 import sys
 
@@ -21,16 +22,24 @@ from sys import platform
 
 def get_input_device_id(p):
   """Returns user selection of available input devices, using PyAudio."""
+  device_id = None
   info = p.get_host_api_info_by_index(0)
   num_devices = info.get('deviceCount')
   print('Available input devices:')
   for i in range(num_devices):
     if p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels') > 0:
       device_name = p.get_device_info_by_host_api_device_index(0, i).get('name')
-      print(f'{i}: {device_name}')
-  # Prompt user to enter input device ID
-  device_id = input('Enter input device ID: ')
-  return int(device_id)
+      if platform == 'linux' and device_name == 'default':
+        print(f'{i}: {device_name}  (recommended)')
+        device_id = i
+      else:
+        print(f'{i:2d}: {device_name}')
+  if not device_id:
+    # Prompt user to enter input device ID
+    device_id = input('Enter input device ID: ')
+    return int(device_id)
+  else:
+    return device_id
 
 
 def count_zero_crossings(audio):
@@ -75,47 +84,66 @@ def add_color_escape_code(text_string, color='black'):
 
 def main():
   parser = argparse.ArgumentParser(
-    description='Time-domain signal processing on microphone audio stream.')
+    description=('Input device time-domain signal analysis: '
+                 'blue color indicates possible speech; '
+                 'red color indicates clipping saturation.'))
   parser.add_argument('-b', '--buffer_time', type=float, default=1.0,
-                      help='Signal buffer time in seconds, S.')
+                      help=('Signal buffer time (buffer is filled with chunks)'
+                            ', seconds.'))
   parser.add_argument('-c', '--chunk_size', type=int, default=1600,
                       help='Audio stream chunk size.')
-  parser.add_argument('--crest_factor_threshold', type=float, default=9.0,
-                      help=('Threshold value for the buffer crest factor, '
-                            'e.g.: 9.'))
   parser.add_argument('-d', '--device', type=int, default=None,
-                      help='Input device ID, script will prompt.')
+                      help='Input device ID.')
   parser.add_argument('--filter_highpass', default=True,
                       action=argparse.BooleanOptionalAction,
                       help='Remove low frequency from the audio.')
-  parser.add_argument('-p', '--plot', default=False,
+  parser.add_argument('-g', '--gain_software', type=float, default=None,
+                      help=('Gain applied in software to audio (it is not '
+                            'applied to red color clipping saturation '
+                            'detection), dB. i.e.: -g 20'))
+  parser.add_argument('-p', '--plot', default=True,
                       action=argparse.BooleanOptionalAction,
-                      help=('Show plot on Mac (warning: plotting fails on '
-                            'Ubuntu 22.04 LTS with Python 3.10.9).'))
+                      help=('Show plot (recommended Linux input device is '
+                            '"default" to mitigate stream crash).'))
   parser.add_argument('-r', '--rms_threshold', type=float, default=-50.,
-                      help=('Threshold value for the buffer rms level in dB, '
-                            'e.g.: -50'))
+                      help=('Threshold value for the buffer rms level, dB. '
+                            'i.e.: -r -50'))
   parser.add_argument('-s', '--sampling_frequency', type=int, default=16000,
-                      help=('Audio stream sampling frequency in hertz, Hz '
-                            '(warning: values other than 48000 fail on '
-                            'Ubuntu 22.04 LTS with Python 3.10.9).'))
-  parser.add_argument('-z', '--zero_crossings', default=False,
+                      help=('Audio stream sampling frequency in hertz, Hz.'))
+  parser.add_argument('--crest_factor_thresholds', type=float,
+                      default=[9., 23.], nargs='+',
+                      help=('Threshold value bounds for the crest factor '
+                            'check.  Within these two values the signal may be '
+                            'speech, i.e.: --crest_factor_thresholds 9. 23.'))
+  parser.add_argument('--zero_crossings', default=True,
                       action=argparse.BooleanOptionalAction,
                       help=('Include zero crossings when analyzing energy '
                             '(this can catch non-speech noises such as '
                             'high frequency "hissing", or steady low frequency '
                             'tones).'))
+  parser.add_argument('--zero_crossing_thresholds', type=float,
+                      default=[400., 6000.], nargs='+',
+                      help=('Threshold value bounds for the zero crossings per '
+                            'second check applied when -z is True. Within '
+                            'these two values the signal may be speech. '
+                            'i.e.: --zero_crossing_thresholds 400. 6000.'))
   args = parser.parse_args()
 
+  # TODO(guy): add more assertion tests on arguments.
   assert args.rms_threshold <= 0.
-  assert args.crest_factor_threshold > 0.
+  for thresholds in [args.crest_factor_thresholds,
+                     args.zero_crossing_thresholds]:
+    assert len(thresholds) == 2
+    for item in thresholds:
+      assert item > 0.
+    assert (thresholds[1] - thresholds[0]) > 0.
 
-  # Parameters for audio capture
+  # Parameters for input device audio capture.
   FORMAT = pyaudio.paFloat32
   CHANNELS = 1
 
   if args.device is None:
-    # Get input device ID from user if not provided as argument
+    # Get input device ID from user if not provided as argument.
     p = pyaudio.PyAudio()
     device_id = get_input_device_id(p)
     p.terminate()
@@ -126,43 +154,54 @@ def main():
     # Design coefficients for a high pass filter.
     b, a  = signal.butter(4, 60, 'highpass', fs=args.sampling_frequency)
 
-  # Create PyAudio object and select input device
+  # Create PyAudio object and select input device.
   p = pyaudio.PyAudio()
   device_info = p.get_device_info_by_index(device_id)
 
+  # Check format is supported.
+  assert p.is_format_supported(
+    input_device=device_id, input_format=FORMAT,
+    input_channels=CHANNELS, rate=args.sampling_frequency,
+  )
+
   if args.plot:
-    # Initialize plot figure
+    # Initialize plot figure.
     plt.figure()
     plt.pause(0.001)
 
-  # Open microphone stream
+  # Open input device stream.
   stream = p.open(format=FORMAT, 
                   channels=CHANNELS, rate=args.sampling_frequency,
                   input=True, input_device_index=device_info['index'], 
                   frames_per_buffer=args.chunk_size)
 
-  # Initialize buffer variables
+  # Initialize buffer variables.
   signal_buffer = np.zeros(int(args.sampling_frequency * args.buffer_time))
   plot_index = 0
 
-  if platform == 'linux':
+  if platform == 'linux' and device_info['name'] != 'default': 
+    print('\nWhen not using "default" Linux input device:')
     if args.plot: 
-      print('WARNING: plotting on Linux may cause crash, try --no-plot')
+      print('  WARNING: plotting may cause crash, try --no-plot')
     if args.sampling_frequency != 48000:
-      print(f'WARNING: sampling_frequency {args.sampling_frequency} on Linux '
-             'may cause crash, try -s 48000')
+      print(f'  WARNING: sampling_frequency {args.sampling_frequency} may '
+             'cause crash, try -s 48000')
 
   print(
-    'Basic analysis method attempts to identify speech-like signals '
-    '(blue color).\nPress Ctrl-c to quit...')
+"""
+Time domain analysis:
+  Stream clipping saturation (red color)
+  Possible speech signals (blue color)
+Press Ctrl-C to quit.
+""")
 
-  # Loop to continuously capture and process audio
+  # Loop to continuously capture and process audio.
   try:
     while True:
-      # Read audio data from microphone stream
+      # Read audio data from stream.
       audio_data = stream.read(args.chunk_size)
 
-      # Convert audio data to numpy array
+      # Convert audio data to numpy array.
       audio_buffer = np.frombuffer(audio_data, dtype=np.float32)
 
       # Add new chunk to the buffer.
@@ -172,33 +211,43 @@ def main():
 
       # Display text and plot.
       if plot_index >= int(args.sampling_frequency * args.buffer_time):
+        peak_signal = peak(signal_buffer)
 
         # Filter the signal buffer to remove low frequencies.
-        audio = signal_buffer
+        audio = signal_buffer        
         if args.filter_highpass:
-          audio = signal.lfilter(b, a, signal_buffer)
+          audio = signal.lfilter(b, a, audio)
 
-        # Simple analysis of signal.
+        # Apply software gain to adjust signal level.
+        if args.gain_software:
+          # TODO(guy): check INT16.
+          audio = audio * (10. ** (args.gain_software / 20.0))
+
+        # Simple analysis of processed signal.
         rms = root_mean_square(audio)
         pk = peak(audio)
         crest_factor = pk - rms
-        zero_crossings = count_zero_crossings(audio)
-        normalized_zero_crossings = int(zero_crossings / args.buffer_time)
+        zero_crossings_per_second = int(
+          count_zero_crossings(audio) / args.buffer_time)
 
-        text_str = (f'RMS:{rms:>+2.1f}dB  '
-                    f'Peak:{pk:>+2.1f}dB  '
+        text_str = (f'RMS:{rms:>+5.1f}dB  '
+                    f'Peak:{pk:>+5.1f}dB  '
                     f'C.F.:{crest_factor:>4.1f}  '
-                    f'Z.C.:{normalized_zero_crossings:>5d}')
+                    f'Z.C.:{zero_crossings_per_second:>5d}')
         
         # Apply heuristic analysis to find signals with energy.
         # These choices selected on 'MacBook Pro Microphone' with
         # Settings > Sound > Input level set at default (0.5).
         colour = 'black'
-        if (rms > args.rms_threshold and 
-            crest_factor > args.crest_factor_threshold and
+        if peak_signal > 0.:
+          colour = 'red'
+        elif (rms > args.rms_threshold and 
+            (crest_factor >= args.crest_factor_thresholds[0] and
+             crest_factor <= args.crest_factor_thresholds[1]
+            ) and
             (not args.zero_crossings or 
-             (normalized_zero_crossings > 400 and 
-              normalized_zero_crossings < 5000))
+             (zero_crossings_per_second >= args.zero_crossing_thresholds[0] and 
+              zero_crossings_per_second <= args.zero_crossing_thresholds[1]))
             ):
           colour = 'blue'  
         
@@ -221,7 +270,7 @@ def main():
   except KeyboardInterrupt:
     print('\nCtrl-C exception, cleaning up resources.')
 
-  # Clean up resources
+  # Clean up resources.
   stream.stop_stream()
   stream.close()
   p.terminate()
